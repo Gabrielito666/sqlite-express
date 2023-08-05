0bbe67023e10361c62d219217aecee00fbd211f3
Gabrielito666 <galileo.fandango@gmail.com>
2023-08-05 16:14:19 -0400

    Corrección de errores

--- modules/submodules/is.js
+++ modules/submodules/is.js
@@ -1,9 +1,9 @@
 module.exports = {
     o : (x)=>{return typeof x === 'object' && x !== null},
     a : (x)=>{return Array.isArray(x)},
-    b : (x)=>{return typeof parametro === 'boolean'},
+    b : (x)=>{return typeof x === 'boolean'},
     s : (x)=>{return typeof x === 'string'},
     n : (x)=>{return typeof x === 'number'},
-    j : (x)=>{if(!this.s(x)){return false}try{JSON.parse(x)}catch(err){return false}return true},
+    j : (x)=>{if(typeof x !== 'string'){return false}try{JSON.parse(x)}catch(err){return false}return true},
     f : (x)=>{return typeof x === 'function'},
 }
